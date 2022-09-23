3481c913819444c455f426201463b7f2bb49ba32
Elyes Saddem <saddemelyes67@gmail.com>
2022-09-23 18:34:37 +0200

    RandomizeProblems

---
+++ index.js
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch';
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv'
+dotenv.config();
+const client = new Client({
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+});
+var problem = "";
+const getRandomProblem = (data) => {
+    console.log(data);
+    const randomNumber = Math.floor(Math.random() * (data.stat_status_pairs.length));
+    if (data.stat_status_pairs[randomNumber].paid_only)
+        getRandomProblem(data);
+    else 
+    {
+        const channel = client.channels.cache.find(channel => channel.name === 'problem-solving');
+        channel.send("https://leetcode.com/problems/"+data.stat_status_pairs[randomNumber].stat.question__title_slug);
+    }
+
+}
+client.on("ready",() =>console.log("rdy"))
+client.on("messageCreate", async (message) =>{
+    console.log(message)
+if (message.content === "-leetcode start") {
+
+    fetch("https://leetcode.com/api/problems/all/").then(response => response.json())
+    .then(data => {
+        getRandomProblem(data);
+
+    })
+}
+
+})
+client.login(process.env.ENV_VARIABLE);
